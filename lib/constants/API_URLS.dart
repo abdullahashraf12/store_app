a9e7d28baa15f477487ff09688dca79455832053
Abdullah Ashraf <alpha1.abdullah@gmail.com>
2024-03-21 05:21:56 +0200

    SearchUrlAdded

--- lib/constants/API_URLS.dart
+++ lib/constants/API_URLS.dart
@@ -1,26 +1,20 @@
-abstract class ApiUrls {
-  static String ip = "192.168.1.11";
-  static String port = "8080";
-  static String getAllProductsUrl = "http://" + ip + ":" + port + "/mobApi/a_p";
-  static String getAllCategoriesUrl =
-      "http://" + ip + ":" + port + "/mobApi/a_c";
-  static String getAllVendorsUrl = "http://" + ip + ":" + port + "/mobApi/a_v";
-  static String getAllProductByIDsUrl =
-      "http://" + ip + ":" + port + "/mobApi/a_p/";
-  static String getAllCategoriesByIDsUrl =
-      "http://" + ip + ":" + port + "/mobApi/a_c/";
-  static String getAllVendorsByIDsUrl =
-      "http://" + ip + ":" + port + "/mobApi/a_v/";
-  static String variable_categ = "";
-  static String variable_prod = "";
+class ApiUrls {
+  static const String ip = "192.168.1.11";
+  static const String port = "8080";
 
-  static String SearchProduct = "http://" +
-      ip +
-      ":" +
-      port +
-      "/mobApi/get_p_n_c/" +
-      "?category_category=" +
-      variable_categ +
-      "&search_name=" +
-      variable_prod;
+  static const String getAllProductsUrl = "http://$ip:$port/mobApi/a_p";
+  static const String getAllCategoriesUrl = "http://$ip:$port/mobApi/a_c";
+  static const String getAllVendorsUrl = "http://$ip:$port/mobApi/a_v";
+
+  static String getAllProductByIDsUrl(String productId) =>
+      "http://$ip:$port/mobApi/a_p/$productId";
+
+  static String getAllCategoriesByIDsUrl(String categoryId) =>
+      "http://$ip:$port/mobApi/a_c/$categoryId";
+
+  static String getAllVendorsByIDsUrl(String vendorId) =>
+      "http://$ip:$port/mobApi/a_v/$vendorId";
+
+  static String searchProductUrl(String category, String productName) =>
+      "http://$ip:$port/mobApi/get_p_n_c/?category_category=$category&search_name=$productName";
 }
